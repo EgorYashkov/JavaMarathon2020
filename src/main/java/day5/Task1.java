3346b1aff9d4e444a4e0bdfe44f790171746ab9d
Egor Yashkov <E.Yashkov.D@yandex.ru>
2024-02-23 19:45:24 +1000

    pobeda

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,51 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car1 = new Car();
+        car1.setYaer(12);
+        car1.setColor("MAMDA");
+        car1.setModel("Ford");
+        System.out.println(car1.getYaer());
+        System.out.println(car1.getYaer());
+        System.out.println(car1.getModel());
     }
 }
+
+class Car {
+    private int yaer;
+    public void setYaer (int usery) {
+        if (usery < 0)
+            System.out.println("-1000");
+        else
+            yaer = usery;
+    }
+    public int getYaer(){
+        return yaer;
+    }
+
+    private String color;
+    public void setColor (String userColor){
+        if (userColor.isEmpty())
+            System.out.println("Пусто");
+        else
+            color = userColor;
+    }
+    public String getColor(){
+        return color;
+    }
+    private String model;
+    public void setModel (String userModel){
+        if (userModel.isEmpty())
+            System.out.println("Пусто");
+        else
+            model = userModel;
+    }
+    public String getModel(){
+        return model;
+    }
+
+
+}
+
+
+
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,30 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        motorbike motorbike1 = new motorbike(12,"green", "opa");
+        motorbike1.getInfo();
     }
+}
+
+class motorbike {
+    private int startyesrs;
+    private String color;
+    private String model;
+    public motorbike(){
+        System.out.println("где данные");
+    }
+
+
+
+    public  motorbike (int startyesrs, String color, String model){
+        this.startyesrs = startyesrs;
+        this.color = color;
+        this.model = model;
+    }
+    public void getInfo(){
+        System.out.println(startyesrs);
+        System.out.println(color);
+        System.out.println(model);
+    }
+
 }
